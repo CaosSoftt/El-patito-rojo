b2b5205a88183543873bd1d48f3ad424188cd23c
Alanbalza <107290740+Alanbalza@users.noreply.github.com>
2023-10-23 20:15:06 -0600

    Agregacion de graficas

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,12 +2,15 @@ import Header from "../componentes/atomos/Header";
 import Footer from "../componentes/atomos/footer";
 import DataTable from "../componentes/moleculas/DataTable";
 import Login from "../componentes/atomos/Login";
+import Graficas from "../componentes/moleculas/Graficas";
+
 
 function Dashboard() {
     return (
         <div>
             <Header></Header>
             <Login></Login>
+            <Graficas></Graficas>
             <DataTable></DataTable>
             <Footer></Footer>
         </div>
